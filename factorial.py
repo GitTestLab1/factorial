1d32d196374721c887c4387b0465e309aca7e535
GitTest <44243500+GitTestLab1@users.noreply.github.com>
2023-02-23 00:08:21 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -22,7 +22,7 @@ def sum_factorial():
 
     result=sum(final_list)
 
-    print("Final SUM isissdsdjjs {}".format(result))
+    print("Final SUM isissdsdjjs zezez{}".format(result))
 
     return result
 
